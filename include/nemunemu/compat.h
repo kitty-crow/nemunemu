ae881448a7141745bf48bb8ea4a429a62c38c37a
Kitty <141717449+kitty-crow@users.noreply.github.com>
2026-07-24 22:20:52 +0100

    Keep NEMUNEMU as Linux PID 1

--- include/nemunemu/compat.h
+++ include/nemunemu/compat.h
@@ -11,6 +11,7 @@ extern "C" {
 int nemu_compat_read_marker(const char *script, char *out, size_t out_size);
 int nemu_compat_read_thx(const char *script, char *out, size_t out_size);
 bool nemu_compat_marker_supported(const char *name);
+int nemu_compat_init(const char *root);
 int nemu_compat_shell(const char *root);
 int nemu_compat_marker(const char *script, int argc, char **argv);
 int nemu_compat_thx_wrapper(const char *script, int argc, char **argv);
