d85aa8c45d5b7b59ff25ca91267553abbd3e1493
Kitty <141717449+kitty-crow@users.noreply.github.com>
2026-07-24 11:53:23 +0100

    Dispatch THX2 by instruction profile

--- src/thx.c
+++ src/thx.c
@@ -4,4 +4,13 @@
 #include "thx/30_linux.inc"
 #include "thx/40_vm32.inc"
 #include "thx/45_rv64.inc"
+
+#define run64 run_thistle64
+#define nemu_check_image nemu_check_image_canonical
+#define nemu_run nemu_run_canonical
 #include "thx/50_vm64.inc"
+#undef nemu_run
+#undef nemu_check_image
+#undef run64
+
+#include "thx/55_dispatch.inc"
