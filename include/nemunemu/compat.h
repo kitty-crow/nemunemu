097064ec22dc9e7143486f109a1941e4289c49d3
Kitty <141717449+kitty-crow@users.noreply.github.com>
2026-07-24 11:38:50 +0100

    Expose marker compatibility coverage

--- include/nemunemu/compat.h
+++ include/nemunemu/compat.h
@@ -1,6 +1,7 @@
 #ifndef NEMUNEMU_COMPAT_H
 #define NEMUNEMU_COMPAT_H
 
+#include <stdbool.h>
 #include <stddef.h>
 
 #ifdef __cplusplus
@@ -9,6 +10,7 @@ extern "C" {
 
 int nemu_compat_read_marker(const char *script, char *out, size_t out_size);
 int nemu_compat_read_thx(const char *script, char *out, size_t out_size);
+bool nemu_compat_marker_supported(const char *name);
 int nemu_compat_shell(const char *root);
 int nemu_compat_marker(const char *script, int argc, char **argv);
 int nemu_compat_thx_wrapper(const char *script, int argc, char **argv);
